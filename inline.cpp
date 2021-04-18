d0db76d01a81363600f1b9725a744988a1bb9dd4
Dharmendra Mehta <dharmendramehta622@gmail.com>
2021-04-18 20:55:36 +0545

    Inline Function

---
+++ inline.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+inline int max(int x, int y){
+    return ((x>y)? x : y);
+}
+
+int main(){
+    int a, b;
+    cout << "Enter the value of a & b:";
+    cin >> a >> b;
+    cout << "Greater value is: " << endl;
+    max(a, b);
+
+    getch();
+    return 0;
+}
